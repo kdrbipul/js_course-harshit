ac382714ff95727cfd3742e69a329c137578981c
kdrbipul <kdrbipul@gmail.com>
2023-09-23 15:34:17 +0600

    Double Equal & Triple Equal check

--- 08.js
+++ 08.js
@@ -20,17 +20,25 @@
 // console.log(plus, plus2);
 // console.log(typeof plus2);
 
-const name = 'Abdul Kader';
-const age = 23;
-const aboutMe = `My name is ${name} and my age is ${age}`;
-console.log(aboutMe);
+// const name = 'Abdul Kader';
+// const age = 23;
+// const aboutMe = `My name is ${name} and my age is ${age}`;
+// console.log(aboutMe);
 
-let x;
-console.log(x);
+// let x;
+// console.log(x);
 
-const myNumber = BigInt(12)
-const newMyNumber = 12n;
-const total = myNumber + newMyNumber;
+// const myNumber = BigInt(12)
+// const newMyNumber = 12n;
+// const total = myNumber + newMyNumber;
 
-console.log(typeof myNumber);
-console.log(total);
+// console.log(typeof myNumber);
+// console.log(total);
+
+const num1 = 20;
+const num2 = 20;
+console.log(typeof (num1 == num2));
+
+const num3 = 20;
+const num4 = '34';
+console.log(num3 === num4);
