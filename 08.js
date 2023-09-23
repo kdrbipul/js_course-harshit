14112afdfef8fd2db1433197839a5e53a6114b32
kdrbipul <kdrbipul@gmail.com>
2023-09-23 13:08:57 +0600

    string to number convert

--- 08.js
+++ 08.js
@@ -2,6 +2,8 @@ const age = 22;
 const firstName = 'Abdul Kader';
 // console.log(typeof age, typeof firstName);
 
-console.log(typeof (age +''));
+// console.log(typeof (age +''));
 
 
+const myAge = '22';
+console.log(typeof (+myAge));
