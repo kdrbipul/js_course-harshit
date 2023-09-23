fef69453714fe8b49207611edfa6bf2f7088ecd5
kdrbipul <kdrbipul@gmail.com>
2023-09-23 10:58:38 +0600

    second add

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const firstName = '   Abdul Kader   '
-console.log(firstName.length);
-const newString = firstName.trim()
-console.log(newString.length);
-console.log(firstName[5])
+const firstName = 'Abdul Kader'
+// console.log(firstName.length);
+// const newString = firstName.trim()
+// console.log(newString.length);
+// console.log(firstName[5])
+
+firstName.toUpperCase()
+console.log(firstName.toUpperCase());
